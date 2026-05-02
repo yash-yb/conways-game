9002bac120d178a421e255b563ad5fdfa884f400
sanskar-sol <sanskarvirat3@gmail.com>
2026-05-02 18:46:28 +0530

    refactor draw_window function to accept parameters for alive cells and camera position

--- main.py
+++ main.py
@@ -6,17 +6,43 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Conway Set the Sail(DEV)")
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+ORIGIN_DOT = (0, 0)
+
 
 FPS = 60
 
-def draw_window():
+def draw_window(alive_cells, camera_x, camera_y, cell_size):
     WIN.fill(BLACK)
+    
+    #for defining the co-ordinates wrt to camera
+    for (x , y) in alive_cells:
+        screen_x = (x * cell_size) + camera_x - (cell_size // 2)
+        screen_y = (y * cell_size) + camera_y - (cell_size // 2) 
+        #the substraction of half of cell size is to center the box on the exact coordinates of camera
+        
+        #drawing the alive cells
+        pygame.draw.rect(WIN, WHITE, (screen_x, screen_y, cell_size, cell_size))
+    
+    # test crosshair
+    pygame.draw.rect(WIN, RED, (camera_x, camera_y, 1, 100))
+    pygame.draw.rect(WIN, RED, (camera_x, camera_y, 100, 1))
+    
+    
     pygame.display.update()
     
-def main():
+def main(): 
+
     clock = pygame.time.Clock()
     run = True
     
+    # initial data for the cells    
+    cell_size = 20
+    camera_x = WIDTH // 2
+    camera_y = HEIGHT // 2
+    alive_cells = {(0, 0), (1, 1), (10, 2)}
+
     while run:
         clock.tick(FPS)
         
@@ -24,7 +50,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 
-        draw_window()
+        draw_window(alive_cells, camera_x, camera_y, cell_size)
     
     pygame.quit()
     
